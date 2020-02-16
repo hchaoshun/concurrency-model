4d4486e75c82fd00c2bdae92538f6f76ef32ab69
Jachin Huang <hcsxiaohan@gmail.com>
2020-02-16 13:46:14 +0800

    modify stop and wait

--- tcpecho/strcli/stopandwait.c
+++ tcpecho/strcli/stopandwait.c
@@ -3,41 +3,15 @@
 void
 str_cli(FILE *fp, int sockfd)
 {
-	int			maxfdp1, stdineof;
-	fd_set		rset;
-	char		buf[MAXLINE];
-	int		n;
+	char	sendline[MAXLINE], recvline[MAXLINE];
 
-	stdineof = 0;
-	FD_ZERO(&rset);
-	for ( ; ; ) {
-		if (stdineof == 0)
-			FD_SET(fileno(fp), &rset);
-		FD_SET(sockfd, &rset);
-		maxfdp1 = max(fileno(fp), sockfd) + 1;
-		Select(maxfdp1, &rset, NULL, NULL, NULL);
+	while (Fgets(sendline, MAXLINE, fp) != NULL) {
 
-		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
-			if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
-				if (stdineof == 1)
-					return;		/* normal termination */
-				else
-					err_quit("str_cli: server terminated prematurely");
-			}
+		Writen(sockfd, sendline, strlen(sendline));
 
-			Write(fileno(stdout), buf, n);
-		}
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
 
-		if (FD_ISSET(fileno(fp), &rset)) {  /* input is readable */
-			if ( (n = Read(fileno(fp), buf, MAXLINE)) == 0) {
-				stdineof = 1;
-				Shutdown(sockfd, SHUT_WR);	/* send FIN */
-				FD_CLR(fileno(fp), &rset);
-				continue;
-			}
-
-			Writen(sockfd, buf, n);
-		}
+		Fputs(recvline, stdout);
 	}
 }
-
